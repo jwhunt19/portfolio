8c2016ed8bd845294347452388f74e6b26ea7a91
Joey Huntington <jwhunt19@gmail.com>
2021-04-09 17:22:02 -0400

    Add material ui modals for certs and project

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,47 @@
 import Image from 'next/image'
+import React, { useState } from 'react';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 export default function Home() {
+  const [gohulaOpen, setGohulaOpen] = useState(false);
+  const [treehouseOpen, setTreehouseOpen] = useState(false);
+  const [galvanizeOpen, setGalvanizeOpen] = useState(false);
+
+  const handleGohula = () => {
+    setGohulaOpen(!gohulaOpen);
+  };
+
+  const handleTreehouse = () => {
+    setTreehouseOpen(!treehouseOpen);
+  };
+
+  const handleGalvanize = () => {
+    setGalvanizeOpen(!galvanizeOpen);
+  };
+
+  const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+
+    paper: {
+      position: 'absolute',
+      maxHeight: '90%',
+      maxWidth: '70%',
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      borderRadius: '5px',
+      outline: 'none',
+    },
+
+  }));
+
+  const classes = useStyles();
+
   return (
     <>
     <header>
@@ -55,25 +96,92 @@ export default function Home() {
       <h3>Experience</h3>
       <div className="experience-wrapper">
         <div className="experience-card">
-          <img className="experience-cert" src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/17097430"></img>
+          <Image
+            onClick={handleTreehouse}
+            className="experience-cert"
+            src="/images/treehouse-cert.png"
+            height={400}
+            width={480}
+            alt="galvanize and hack reactor certificate"
+          />
+          <Modal
+            className={classes.modal}
+            open={treehouseOpen}
+            onClose={handleTreehouse}
+          >
+            <Image
+              className={classes.paper, `modal`}
+              alt="randomly generated"
+              src="/images/treehouse-cert.png"
+              height={720}
+              width={864}
+            />
+          </Modal>
           <h4>Treehouse</h4>
           <p>Front End Web Development Techdegree</p>
         </div>
         <div className="experience-card">
           <Image
+            onClick={handleGalvanize}
             className="experience-cert"
             src="/images/galvanize-cert.png"
             height={400}
             width={480}
             alt="galvanize and hack reactor certificate"
           />
+          <Modal
+            className={classes.modal}
+            open={galvanizeOpen}
+            onClose={handleGalvanize}
+          >
+            <Image
+              className={classes.paper, `modal`}
+              alt="randomly generated"
+              src="/images/galvanize-cert.png"
+              height={720}
+              width={864}
+            />
+          </Modal>
           <h4>Hack Reactor | Galvanize</h4>
-          <p>Front End Web Development Techdegree</p>
+          <p>Advanced Software Engineering Immersive</p>
         </div>
       </div>
     </div>
 
-    {/*  */}
+    {/* Projects */}
+    <div className="projects">
+      <h3>Projects</h3>
+      <div className="projects-card">
+      <Image
+          onClick={handleGohula}
+          className="projects-picture"
+          src="/images/gohula.gif"
+          height={280}
+          width={480}
+          alt="galvanize and hack reactor certificate"
+        />
+        <Modal
+          className={classes.modal}
+          open={gohulaOpen}
+          onClose={handleGohula}
+        >
+          <Image
+            className={classes.paper, `modal`}
+            alt="randomly generated"
+            src="/images/gohula.gif"
+            height={700}
+            width={1200}
+          />
+        </Modal>
+        <div className="projects-card-text">
+          <h4>GoHuLa</h4>
+          <p><b><b></b>Tech Stack:</b> MongoDB, Mongoose, Express, React, Node.js, Axios, Docker, 
+          Bootstrap, Material-UI, React Testing Library, Jest, Supertest</p>
+          <br></br>
+          <p>A mock up</p>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
