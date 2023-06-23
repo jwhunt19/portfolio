24f6cd611af3f39ebd013f4cbf144cb61d1c4b8f
Joey Huntington <jwhunt19@gmail.com>
2023-06-22 19:43:42 -0600

    add remark

---
+++ pages/posts/[id].js
@@ -0,0 +1,23 @@
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
