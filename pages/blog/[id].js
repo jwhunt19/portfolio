540f4ecf91d8a7c4af94a81cb0f3f7480a64a334
Joey Huntington <jwhunt19@gmail.com>
2024-01-02 19:18:07 -0700

    rename layout to bloglayout

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../src/components/layout.js'
+import Layout from '../../src/components/blogLayout.js'
 import { getPostIDs, getPostData } from '../../lib/posts.js'
 import styles from './blog.module.css';
 
