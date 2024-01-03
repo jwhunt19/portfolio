ffb52561aaf5302d6b81cc1ede00090e7a2dcd48
Joey Huntington <jwhunt19@gmail.com>
2024-01-02 21:43:11 -0700

    move modal handler to index.js

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,18 +1,14 @@
 import { useState } from "react";
 import Project from "./Project";
 
-const ProjectList = ({ projects, classes }) => {
-  const [openModalId, setOpenModalId] = useState(null);
-
-  const openModal = (projectId) => setOpenModalId(projectId);
-  const closeModal = () => setOpenModalId(null);
+const ProjectList = ({ projects, classes, openModalId, openModal, closeModal }) => {
 
   return (
     <div id="projects" className="projects">
       <h3>Projects</h3>
       {projects.map((project) => (
         <Project
-          key={project.id}
+          key={project.name}
           project={project}
           openModalId={openModalId}
           openModal={openModal}
